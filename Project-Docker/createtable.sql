ae48b1efe86f798a02cd451343ae636bdc5eb093
Aamir041 <aamirsaudagar34@gmail.com>
2023-03-16 23:53:34 +0530

    First Docker Project added

---
+++ Project-Docker/createtable.sql
@@ -0,0 +1,5 @@
+create table mit1 (firstname varchar(20), lastname varchar(20));
+create table mit2 (firstname varchar(20), lastname varchar(20));
+create table mit3 (firstname varchar(20), lastname varchar(20));
+create table mit4 (firstname varchar(20), lastname varchar(20));
+create table mit5 (firstname varchar(20), lastname varchar(20));
---
+++ Project-Docker/insertvalues.sql
@@ -0,0 +1,4 @@
+insert into mit1 values ('Aamir','Saudagar');
+insert into mit1 values ('Owais','Saudagar');
+insert into mit1 values ('Alipasha','Saudagar');
+insert into mit1 values ('Seema', 'Saudagar');
